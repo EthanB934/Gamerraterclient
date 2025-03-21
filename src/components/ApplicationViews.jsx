46307415f4135f03174e7319d9774243f6226af8
Ethan Brown <eb934053@gmail.com>
2025-03-21 14:28:59 -0500

    Deleted unused import statements

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Authorized } from "./Authorized"
 import { Login } from "../pages/Login.jsx"
 import Home from "../pages/Home"
-import { RockForm } from "./RockForm.jsx"
-import { RockList } from "./RockList.jsx"
 import { Register } from '../pages/Register.jsx'
 
 
@@ -35,9 +33,6 @@ export const ApplicationViews = () => {
             <Route path="/register" element={<Register />} />
             <Route element={<Authorized />}>
                 <Route path="/" element={<Home />} />
-                <Route path="/games" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/review" element={<RockForm fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/mine" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
             </Route>
         </Routes>
     </BrowserRouter>
