b3843b1592306ec691e5debdf432349fa7c35997
Ethan Brown <eb934053@gmail.com>
2025-03-25 16:04:08 -0500

    Routed games to their details page

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -1,15 +1,42 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Authorized } from "./Authorized";
 import { Login } from "../pages/Login.jsx";
-import Home from "../pages/Home";
 import { Register } from "../pages/Register.jsx";
 import { useEffect, useState } from "react";
+import { GamesList } from "./Games/GamesList.jsx";
 
 export const ApplicationViews = () => {
   const [token, setToken] = useState({});
+  const [games, setGames] = useState([])
+  // Gets user-specific token from local storage after login or registration process
   useEffect(() => {
     setToken(JSON.parse(localStorage.getItem("gamer_token")));
   }, []);
+
+  // GETs all game resources from database, user must be logged in before a response is given
+  const getAllGames = async () => {
+    const gamesResponse = await fetch("http://localhost:8000/games", {
+      method: "GET",
+      headers: {
+        "Authorization": `Token ${token.token}`,
+        Accept: "application/json"
+      }
+    })
+
+    // Resolves promises of API fetch
+    const games = await gamesResponse.json()
+
+    setGames(games)
+  }
+
+  useEffect(() => {
+    if(token.token) {
+      getAllGames()
+    }
+    else {
+      return;
+    }
+  }, [token])
 
   return (
     <BrowserRouter>
@@ -17,7 +44,7 @@ export const ApplicationViews = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route element={<Authorized />}>
-          <Route path="/" element={<Home token={token}/>} />
+          <Route path="/" element={<GamesList token={token} games={games}/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Games/GamesList.jsx
+++ src/components/Games/GamesList.jsx
@@ -1,3 +1,20 @@
-export const GamesList = () => {
-    return <>This is where the list of added games will be</>
-}
+import { Link } from "react-router-dom";
+
+export const GamesList = ({ token, games }) => {
+  const listGames = (games) => {
+    const gamesList = games.map((game) => {
+      return <li key={game.id} className="game-card"><Link to={`/games/${game.id}`}>{game.title}</Link></li>;
+    });
+    gamesList.join(" ");
+    return gamesList;
+  };
+  return (
+    <article className="all-games">
+      {token && games ? (
+        <ul className="games-list">{listGames(games)}</ul>
+      ) : (
+        "Waiting for games..."
+      )}
+    </article>
+  );
+};
