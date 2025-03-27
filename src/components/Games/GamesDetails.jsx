cb8ae9b7ad4fd3b8e75281a4a0f9853b338884d0
Ethan Brown <eb934053@gmail.com>
2025-03-27 14:33:20 -0500

    Styled details page

--- src/components/Games/GamesDetails.jsx
+++ src/components/Games/GamesDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-
+import "./GamesDetails.css";
 export const GameDetails = ({ token, games }) => {
   const { gameId } = useParams();
 
@@ -7,20 +7,24 @@ export const GameDetails = ({ token, games }) => {
     const game = games.find((game) => game.id === parseInt(gameId));
     return (
       <>
-        <article>
-          <section>
-            <h1>{game.title}</h1>
-            <p>{game.description}</p>
-            <p>Designed by {game.designer}</p>
-            <ul>
+        <article className="game-details">
+          <h1 className="game-title">{game.title}</h1>
+          <section className="details">
+            <p className="game-description">{game.description}</p>
+            <p className="game-designer">Designed by {game.designer}</p>
+            <ul className="game-category-list">
               {game.categories.map((category) => {
-                return <li key={category.id}>{category.name}</li>
+                return (
+                  <li className="game-category" key={category.id}>
+                    {category.name}
+                  </li>
+                );
               })}
             </ul>
-            <footer>
-              Released in: {game.year_released} Total players:{" "}
-              {game.player_count} Played for: {game.play_time} hours Recommended
-              age: {game.age_to_play}
+            <footer className="details-footer">
+              |{" "}Released in: <strong>{game.year_released}</strong>{" "}|{" "}Total players:
+              {" "}<strong>{game.player_count}</strong>{" "}| Estimated play time <strong>{game.play_time}</strong> hours{" "}|{" "}Recommended
+              age: <strong>{game.age_to_play}</strong>{" "}|
             </footer>
           </section>
         </article>
